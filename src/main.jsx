5d134ae4a85e8c4f4f9139674327e097ed5016d9
Yehia Arabi <yyyyahya852@gmail.com>
2025-09-21 20:27:45 +0300

    check of understood

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById('root')
+ReactDOM.createRoot(root).render(<h1>Hello world</h1>)
